62a36b23e462f72dd044477c5873cce80e07593e
E-Kusatake <eiichi1198@gmail.com>
2022-06-03 19:48:07 +0900

    [Add] index

---
+++ javamaximin/MyEdge.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2p_maximin;
+
+/**
+ *
+ * @author imahori
+ */
+public class MyEdge {
+    int label;
+    double x;
+    MyNode ini, ter;
+    
+    public MyEdge(int label, MyNode ini, MyNode ter, double x){ // label, flow, initial node, terminal node
+        this.label = label;
+        this.x = x;
+        this.ini = ini;
+        this.ter = ter;
+    }
+}
